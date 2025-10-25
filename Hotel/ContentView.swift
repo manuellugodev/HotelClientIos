5469f27b320a26b82a797d82a003f7f41122eb5c
Manuel <manuellugo2000ml@gmail.com>
2025-10-25 11:14:58 -0500

    Initial Commit

---
+++ Hotel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hotel
+//
+//  Created by Manuel Lugo on 10/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hotel/HotelApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelApp.swift
+//  Hotel
+//
+//  Created by Manuel Lugo on 10/25/25.
+//
+
+import SwiftUI
+
+@main
+struct HotelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
